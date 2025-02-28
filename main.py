cc40583b89de5cb82e89f21c5145717f62f1a546
yaswanth <yaswanthapswr1234@gmail.com>
2025-02-28 21:11:57 +0530

    port_added

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from sending_req import send_req_bp
 
-app=Flask(__name__)
-
+app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def homepage():
-    return "Welecome to my chatboat"
-
+    return "Welcome to my chatbot"
 
 app.register_blueprint(send_req_bp)
 
-if __name__=="__main__":
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
